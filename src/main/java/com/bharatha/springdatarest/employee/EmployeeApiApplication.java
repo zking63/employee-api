be3c6a13d11ac558c83accd35bdba8ff2da8deac
zking63 <zoe@DGA-6571.local>
2022-08-08 11:06:29 -0400

    model created

--- src/main/java/com/bharatha/springdatarest/employee/EmployeeApiApplication.java
+++ src/main/java/com/bharatha/springdatarest/employee/EmployeeApiApplication.java
@@ -1,4 +1,4 @@
-package com.HCPAssessment;
+package com.bharatha.springdatarest.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/bharatha/springdatarest/employee/entites/Employee.java
@@ -0,0 +1,43 @@
+package com.bharatha.springdatarest.employee.entites;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String firstName;
+	private String lastName;
+
+	public Long getId() {
+		return id;
+	}
+
+	public Long getResourceId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
